ddb8e1dc9e2b4940905458776b76ae4161888199
mahesh8005github <maheshtavare6@gmail.com>
2024-07-02 22:38:29 +0530

    student class added

---
+++ QuizApplication/src/com/classes/Student.java
@@ -0,0 +1,75 @@
+package com.classes;
+
+public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String city;
+	private String emailId;
+	private String mobileNumber;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + ", username=" + username + ", password="
+				+ password + ", city=" + city + ", emailId=" + emailId + ", mobileNumber=" + mobileNumber + "]";
+	}
+
+}
